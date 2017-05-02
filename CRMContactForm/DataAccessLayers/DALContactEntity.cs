a1b00ea616535af8725bb10f4b45de3ff871c189
zfcoder <fayilzig@gmail.com>
2017-05-02 13:50:01 +0300

    Переделал проект под CRM 4.0.

--- CRMContactForm/DataAccessLayers/DALContactEntity.cs
+++ CRMContactForm/DataAccessLayers/DALContactEntity.cs
@@ -1,6 +1,6 @@
 ﻿using CRMContactForm.Models;
-using Microsoft.Xrm.Client.Services;
-using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk;
+using Microsoft.Crm.SdkTypeProxy;
 
 namespace CRMContactForm.DataAccessLayers
 {
@@ -15,19 +15,39 @@ namespace CRMContactForm.DataAccessLayers
         /// <param name="contactModel">Объект модели контакта.</param>
         public void AddContactToCrm(ContactEntityModel contactModel)
         {
-            var connection =
-                System.Configuration.ConfigurationManager.
-                ConnectionStrings["CrmConnection"].ConnectionString; //Подключение к CRM. Строка подключения указана в файле web.config.
-            using (var service = new OrganizationService(connection))
+            // Получаем токен.
+            var token = new CrmAuthenticationToken
             {
-                var contact = new Entity("contact");
-                contact["fullname"] = contactModel.ContactName;
-                contact["phone"] = contactModel.MobilePhone;
-                contact["email"] = contactModel.Email;
-                contact["jobtitle"] = contactModel.JobTitle;
-                contact["birthday"] = contactModel.BirthDay;
-                service.Create(contact);
+                AuthenticationType = 0,
+                OrganizationName = "AdventureWorksCycle"
+            };
+
+            var connectionString = System.Configuration.ConfigurationManager.
+                ConnectionStrings["CrmConnection"].ConnectionString;
+            
+            // Получаем сервис организации CRM.
+            var service = new CrmService
+            {
+                Url = connectionString,
+                CrmAuthenticationTokenValue = token,
+                Credentials = System.Net.CredentialCache.DefaultCredentials
+            };
+
+            // Создаем новую запись.
+            var newContact = new contact
+            {
+                fullname = contactModel.ContactName,
+                mobilephone = contactModel.MobilePhone,
+                emailaddress1 = contactModel.Email,
+                jobtitle = contactModel.JobTitle
+            };
+
+            if (contactModel.BirthDay.HasValue)
+            {
+                newContact.birthdate = CrmDateTime.FromUser(contactModel.BirthDay.Value);
             }
+
+            service.Create(newContact);
         }
     }
 }
